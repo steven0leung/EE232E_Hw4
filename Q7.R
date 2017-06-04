75497898595ed6c22f8e7c4b5d608e267156bf85
steven <steven0leung@ucla.edu>
2017-06-04 01:07:31 -0700

    make Q7 use daily data

--- Q7.R
+++ Q7.R
@@ -31,18 +31,11 @@ for(i in co_names) { #loop through each company name
   cat("Processing processing" , row_count , "out of", length(co_names),"\n")
   fname_co = paste('finance_data/data/',i,'.csv',sep = "") #get directory of current company name
   co_data_close = read.csv(fname_co,stringsAsFactors = FALSE)$Close # get all closing data
-  co_date = as.Date(read.csv(fname_co,stringsAsFactors = FALSE)$Date,'%Y-%m-%d') # get all dates of data
-  
-  if (is.na(co_date[1])) { # 2 formats of dates in the data files (check which one '%Y-%m-%d'  vs '%m/%d/%y')
-    co_date = as.Date(read.csv(fname_co,stringsAsFactors = FALSE)$Date,'%m/%d/%y') # get all dates of data
-  }
-  
-  mondays_idx = which(weekdays(as.Date(co_date,'%Y-%m-%d')) == "Monday") # find idx of all mondays
-  monday_data = co_data_close[mondays_idx] # find closing value on monday
+ 
   
   r_i_local = numeric(0) # clear variable
-  for (j in 2:length(monday_data)){
-    r_i_local[j-1] = log(monday_data[j]) - log(monday_data[j-1])  #r_i formula
+  for (j in 2:length(co_data_close)){
+    r_i_local[j-1] = log(co_data_close[j]) - log(co_data_close[j-1])  #r_i formula
   }
   
   r_i_mat[[row_count,1]] = i
@@ -85,13 +78,36 @@ for( i in 1 : (length(co_names)-1) ) {
 
 g1 = graph.data.frame(P.data,directed = FALSE)
 
-cat("Number of nodes in the network (Using only Monday data) : ",length(V(g1)),"\n")
-cat("Number of edges in the network (Using only Monday data) : ",length(E(g1)),"\n")
+cat("Number of nodes in the network  : ",length(V(g1)),"\n")
+cat("Number of edges in the network  : ",length(E(g1)),"\n")
+
+nodes = V(g1)$name # vector of all nodes
+sectors = ticker$Sector # vector of all sectors 
+u_sectors = unique(sectors)
+
+node_cols = rep(0,length(nodes))
+col_id = 1
+for(i in u_sectors){
+  node_cols[which(sectors == i)] = col_id #identify colors to unique sectors
+  col_id = col_id + 1
+}
+
+g1_mst = mst(g1 , weights = P.data$weights) # create minimal spanning tree
+
+
+cat("Number of nodes in the network(MST): ",length(V(g1_mst)),"\n") # as a sanity check
+cat("Number of edges in the network(MST): ",length(E(g1_mst)),"\n")
+
+
+plot(g1_mst, vertex.color = node_cols ,
+     vertex.size = rep(7,length(nodes)) , 
+     vertex.label = NA, 
+     main = "Modified Minimal Spanning Tree" ) # mst
 
 # Hist of un-modified P_ij's 
 hist( x= tot_P_ij, breaks = seq(from = min(tot_P_ij), to = max(tot_P_ij), by = (max(tot_P_ij)-min(tot_P_ij))/50), 
-      main = "Histogram of Un-modified P_ij's (Using only Monday Data)", xlab = "d_ij Value", ylab = "Frequency")
+      main = "Histogram of Un-modified P_ij's", xlab = "d_ij Value", ylab = "Frequency")
 
 # Hist of d_ij calculated from modified P_ij
 hist( x= tot_d_ij, breaks = seq(from = min(tot_d_ij), to = max(tot_d_ij), by = (max(tot_d_ij)-min(tot_d_ij))/50), 
-      main = "Histogram of Modified d_ij's (Using only Monday Data)", xlab = "d_ij Value", ylab = "Frequency")
+      main = "Histogram of Modified d_ij's ", xlab = "d_ij Value", ylab = "Frequency")
